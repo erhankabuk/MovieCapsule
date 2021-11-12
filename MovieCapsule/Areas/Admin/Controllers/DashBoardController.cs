d63be09970d92dd2c2ff5f7aa485de54dedd371a
erhankabuk <erhankabuk@hotmail.com>
2021-11-12 13:45:01 +0300

    Admin Area

---
+++ MovieCapsule/Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCapsule.Areas.Admin.Controllers
+{[Area("Admin")]
+    public class DashBoardController : Controller
+    {
+        //Get Admin/Dashboard/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MovieCapsule/Startup.cs
+++ MovieCapsule/Startup.cs
@@ -49,8 +49,12 @@ namespace MovieCapsule
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+           app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+                );
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
